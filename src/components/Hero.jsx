07e5b75ca1ea36441ac955d25e88b74a20857d67
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2025-10-07 23:46:20 +0530

    Hero done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,18 @@ const Hero = () => {
   return (
     <div className="flex flex-col items-center text-8xl font-semibold mt-8">
       <div>Luxurious Interior</div>
-      <div>
-        <img src="" alt="" />
+      <div className="flex justify-center items-center gap-8">
+        <img
+          className="w-45 h-15 rounded-4xl"
+          src="/images/int-h-1.jpg"
+          alt=""
+        />
         <span>And</span>
-        <img src="" alt="" />
+        <img
+          className="w-45 h-15 rounded-4xl"
+          src="/images/int-h-2.jpg"
+          alt=""
+        />
       </div>
       <div>Industrial Design</div>
     </div>
